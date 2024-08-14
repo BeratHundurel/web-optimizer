18f1dd2241bce9ee53a7f9929f1ede6ba6e776ee
Berat Hündürel <berat.hundurel@bilgiedu.net>
2024-08-14 22:33:43 +0300

    Updating return type to json

--- api/src/handlers/minifier_handler.rs
+++ api/src/handlers/minifier_handler.rs
@@ -1,17 +1,18 @@
+use axum::Json;
 use minifier::css::minify as minify_css;
 use minifier::js::minify as minify_js;
 
 use crate::errors::AppError;
 
-pub async fn handle_minify_js(js: String) -> String {
+pub async fn handle_minify_js(js: String) -> Json<String> {
     let minified_js = minify_js(&js).to_string();
-    minified_js
+    Json(minified_js)
 }
 
-pub async fn handle_minify_css(css: String) -> String {
+pub async fn handle_minify_css(css: String) -> Json<String> {
     let minified_css = minify_css(&css)
         .map_err(|_| AppError::MinifyError)
         .unwrap()
         .to_string();
-    minified_css
+    Json(minified_css)
 }
